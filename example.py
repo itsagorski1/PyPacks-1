3f6876e9a2f7c615683ce7eccdcccea70cffb40b
Block120 <bigkirinsgoda@gmail.com>
2025-08-09 16:26:55 -0700

    Edit the example file

--- example.py
+++ example.py
@@ -1,10 +1,12 @@
 import pypacks
 
-# Example usage of PyPacks
-
+# Create the PyPack
 example = pypacks.PyPack()
+
+# Define a function in the PyPack that says "Hello"
 hello = example.function("hello")
 hello.say("Hello")
 
+# Build the PyPack for Bedrock and Java
 example.build(pypacks.PyPackType.JAVA)
 example.build(pypacks.PyPackType.BEDROCK)
