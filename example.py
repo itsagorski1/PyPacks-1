128c902531e48246cb87aaec8c2ab0fd0ce3446a
Block120 <bigkirinsgoda@gmail.com>
2025-08-11 09:40:11 -0400

    Make PyPacks a Python package

--- example.py
+++ example.py
@@ -1,12 +1,18 @@
 import pypacks
 
 # Create the PyPack
-example = pypacks.PyPack()
+example = pypacks.PyPack("example", "Example Pack")
 
-# Define a function in the PyPack that says "Hello"
-hello = example.function("hello")
+# Define a function in the PyPack that says "Hello World!"
+hello = pypacks.Function("hello")
 hello.add_command("say", "Hello")
+hello.add_command("say", "World!")
+hello.attach(example)
+
+# Define another function in the PyPack
+yello = pypacks.Function("yello")
+yello.attach(example)
 
 # Build the PyPack for Bedrock and Java
-example.build(pypacks.PyPackType.JAVA)
-example.build(pypacks.PyPackType.BEDROCK)
+example.build(pypacks.PyPackType.JAVA, "Example")
+example.build(pypacks.PyPackType.BEDROCK, "Example")
